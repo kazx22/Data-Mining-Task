1f0e678bdf7c4f913644d6711d8076ff499a5871
kazx22 <kazialifhaider@gmail.com>
2023-09-26 12:11:54 +0000

    3rd

--- House Market Analyzer.py
+++ House Market Analyzer.py
@@ -4,10 +4,10 @@ def houseCsm(hl):
     for i in range(len(hl)):
         total = 0.0
         for k, v in hl[i].items():
-            if k != "id" and k != "price":
+            if k != "Price":
                 total += v
         houseArea.append(total)
-        csm.append(hl[i]["price"] / total)
+        csm.append(hl[i]["Price"] / total)
         total = 0
 
     return houseArea, csm
@@ -20,7 +20,7 @@ def roomCheck(hl):
     totalNonExisting = []
     for i in range(len(hl)):
         for k, v in hl[i].items():
-            if k != "id" and k != "price":
+            if k != "Price":
                 if v == 0:
                     print(k)
                     nonExisting.append(k)
@@ -34,45 +34,163 @@ def roomCheck(hl):
 
 
 def houseAnalysis(hl):
-    houselist = []
+    houseInfo = []
 
     hc = houseCsm(hl)
     rch = roomCheck(hl)
 
-    for i in range(len(hc)):
+    for i in range(len(hl)):
         houseData = {
-            "id": i + 1,
-            "total": hc[0][i],
-            "cpsm": hc[1][i],
-            "empty": rch[0][i],
+            "Id": i + 1,
+            "Total": hc[0][i],
+            "CPSM": hc[1][i],
+            "EmptyRooms": rch[0][i],
         }
-        houselist.append(houseData)
+        houseInfo.append(houseData)
 
-    return houselist
+    return houseInfo
 
 
 houses = [
     {
-        "id": 1,
-        "price": 185000,
-        "b1": 9.5,
-        "b2": 5,
-        "b3": 0,
-        "lv": 8,
-        "kit": 5,
-        "bath": 1.8,
-        "gar": 12,
+        "Price": 185000,
+        "Bedroom 1": 9.5,
+        "Bedroom 2": 5,
+        "Bedroom 3": 0,
+        "Living-Room": 8,
+        "Kitchen": 5,
+        "Bathroom": 1.8,
+        "Garage": 12,
     },
     {
-        "id": 2,
-        "price": 201999,
-        "b1": 12,
-        "b2": 8,
-        "b3": 4,
-        "lv": 14,
-        "kit": 7,
-        "bath": 3,
-        "gar": 18,
+        "Price": 201999,
+        "Bedroom 1": 12,
+        "Bedroom 2": 8,
+        "Bedroom 3": 4,
+        "Living-Room": 14,
+        "Kitchen": 7,
+        "Bathroom": 3,
+        "Garage": 18,
+    },
+    {
+        "Price": 140000,
+        "Bedroom 1": 0,
+        "Bedroom 2": 0,
+        "Bedroom 3": 0,
+        "Living-Room": 55,
+        "Kitchen": 0,
+        "Bathroom": 0,
+        "Garage": 0,
+    },
+    {
+        "Price": 180000,
+        "Bedroom 1": 11.5,
+        "Bedroom 2": 2,
+        "Bedroom 3": 3,
+        "Living-Room": 6,
+        "Kitchen": 5,
+        "Bathroom": 2,
+        "Garage": 10,
+    },
+    {
+        "Price": 195000,
+        "Bedroom 1": 15.5,
+        "Bedroom 2": 3,
+        "Bedroom 3": 2,
+        "Living-Room": 8,
+        "Kitchen": 5,
+        "Bathroom": 2.5,
+        "Garage": 14,
+    },
+    {
+        "Price": 210000,
+        "Bedroom 1": 14.5,
+        "Bedroom 2": 6,
+        "Bedroom 3": 3,
+        "Living-Room": 12,
+        "Kitchen": 5,
+        "Bathroom": 2,
+        "Garage": 14,
+    },
+    {
+        "Price": 210000,
+        "Bedroom 1": 21.5,
+        "Bedroom 2": 7,
+        "Bedroom 3": 4,
+        "Living-Room": 11,
+        "Kitchen": 5,
+        "Bathroom": 2,
+        "Garage": 14,
+    },
+    {
+        "Price": 205000,
+        "Bedroom 1": 19.5,
+        "Bedroom 2": 4,
+        "Bedroom 3": 6,
+        "Living-Room": 10,
+        "Kitchen": 5,
+        "Bathroom": 2,
+        "Garage": 18,
+    },
+    {
+        "Price": 170000,
+        "Bedroom 1": 1.5,
+        "Bedroom 2": 8,
+        "Bedroom 3": 7,
+        "Living-Room": 10,
+        "Kitchen": 5,
+        "Bathroom": 1.5,
+        "Garage": 0,
+    },
+    {
+        "Price": 175000,
+        "Bedroom 1": 5.5,
+        "Bedroom 2": 10,
+        "Bedroom 3": 8,
+        "Living-Room": 6,
+        "Kitchen": 5,
+        "Bathroom": 1.5,
+        "Garage": 0,
+    },
+    {
+        "Price": 180000,
+        "Bedroom 1": 3.5,
+        "Bedroom 2": 12,
+        "Bedroom 3": 8,
+        "Living-Room": 6,
+        "Kitchen": 5,
+        "Bathroom": 1.8,
+        "Garage": 0,
+    },
+    {
+        "Price": 185000,
+        "Bedroom 1": 8.5,
+        "Bedroom 2": 1,
+        "Bedroom 3": 0,
+        "Living-Room": 6,
+        "Kitchen": 5,
+        "Bathroom": 1.8,
+        "Garage": 8,
+    },
+    {
+        "Price": 185000,
+        "Bedroom 1": 12.5,
+        "Bedroom 2": 4,
+        "Bedroom 3": 2,
+        "Living-Room": 6,
+        "Kitchen": 5,
+        "Bathroom": 1.8,
+        "Garage": 12,
+    },
+    {
+        "Price": 285000,
+        "Bedroom 1": 15.5,
+        "Bedroom 2": 15,
+        "Bedroom 3": 12,
+        "Living-Room": 25,
+        "Kitchen": 15,
+        "Bathroom": 8,
+        "Garage": 14,
     },
 ]
 
